66fb75570bc7aabda1348d7ddeafad1040f4a963
thearctesian <stephen.d.okita@gmail.com>
2025-10-31 00:19:53 -0700

    added anal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,18 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
 	<title>UC Regents WANTED</title>
-	<meta name="description" content="Accountability for UC Regents and their political connections" />
+	<meta
+		name="description"
+		content="Accountability for UC Regents and their political connections"
+	/>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
